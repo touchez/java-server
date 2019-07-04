912d0be21833de6f99b3fa47b602fec8ddb78e9e
Article23399 <240634015@qq.com>
2019-07-04 11:55:28 +0800

    change some config about redis

--- src/main/java/com/iotexample/demo/config/SessionConfig.java
+++ src/main/java/com/iotexample/demo/config/SessionConfig.java
@@ -2,26 +2,29 @@ package com.iotexample.demo.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 600)
 public class SessionConfig {
     //冒号后面的值是没有配置文件时自动装载的值
-    @Value("${spring.redis.hostname:localhost}")
+    @Value("${spring.redis.host:localhost}")
     private String hostName;
     @Value("${spring.redis.port:6379}")
     private int port;
-//    @Value("${redis.password:7xH5zwHqPu2^}")
-//    private String password;
+    @Value("${spring.redis.password:null}")
+    private String password;
 
     @Bean
     public JedisConnectionFactory connectionFactory() {
         JedisConnectionFactory connection = new JedisConnectionFactory();
         connection.setPort(port);
         connection.setHostName(hostName);
-//        connection.setPassword(password);
-        // connection.setDatabase(0);
+        if (password != null && !"null".equals(password)) {
+            connection.setPassword(password);
+        }
+        connection.setDatabase(0);
         return connection;
     }
 }
--- src/main/java/com/iotexample/demo/myredis/RedisPoolFactory.java
+++ src/main/java/com/iotexample/demo/myredis/RedisPoolFactory.java
@@ -1,11 +1,13 @@
 package com.iotexample.demo.myredis;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
+@Slf4j
 @Service
 public class RedisPoolFactory {
     @Autowired
@@ -17,6 +19,7 @@ public class RedisPoolFactory {
         poolConfig.setMaxIdle(redisConfig.getPoolMaxIdle());
         poolConfig.setMaxTotal(redisConfig.getPoolMaxTotal());
         poolConfig.setMaxWaitMillis(redisConfig.getPoolMaxWait() * 1000);
+        log.info("redis host is :{}", redisConfig.getHost());
         JedisPool jp = new JedisPool(poolConfig,redisConfig.getHost(), redisConfig.getPort(),
                 redisConfig.getTimeout() * 1000, redisConfig.getPassword(), 0);
         return jp;
